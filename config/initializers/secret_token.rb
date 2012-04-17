345b785aff1faa6cfb504e2aeb58b98305dfde42
Alex Tandrau <alexandru@tandrau.com>
2012-04-17 22:56:50 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wellsure::Application.config.secret_token = 'c32c7dbf90c5834c9548b31a0aa85070fb9d2c57fab627a30b2407a1b8f67f3dc0cbcce9fc720327fbdd8b53de9550cdad6a2a262e7f59610b9f057779cb1527'
